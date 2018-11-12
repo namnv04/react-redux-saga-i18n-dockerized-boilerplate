b7f7f629d3ceb02543c8c7b250ff070e9dafd6a5
Nam Vuong <namnv04@gmail.com>
2018-11-12 00:33:02 -0500

    Lint on pre commit

--- .eslintrc.js
+++ .eslintrc.js
@@ -18,29 +18,28 @@ module.exports = {
 		"react"
 	],
 	"rules": {
+		"space-in-parens": ["error", "never"],
+		"template-curly-spacing": ["error", "never"],
+		"array-bracket-spacing": [ "error", "never" ],
+		"object-curly-spacing": [ "error", "always" ],
+		"eol-last": ["error", "always"],
+		"computed-property-spacing": [ "error", "never" ],
 		"no-console": "warn",
 		"no-const-assign": "error",
 		"no-this-before-super": "error",
 		"no-undef": "warn",
 		"no-unreachable": "warn",
 		"no-unused-vars": "warn",
+		"no-use-before-define": "error",
+		"no-extra-semi": "error",
+		"prefer-const": "error",
+		"no-return-assign": "error",
+		"no-param-reassign": "error",
 		"constructor-super": "warn",
 		"valid-typeof": "warn",
-		"indent": [
-			"error",
-			2
-		],
-		"linebreak-style": [
-			"error",
-			"unix"
-		],
-		"quotes": [
-			"error",
-			"single"
-		],
-		"semi": [
-			"error",
-			"always"
-		]
+		"indent": ["error",2],
+		"linebreak-style": ["error","unix"],
+		"quotes": ["error","single"],
+		"semi": ["error", "always"]
 	}
 };
